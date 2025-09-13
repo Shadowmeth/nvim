b38e805a157aabecf0a818e11255368ce8f7b81c
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-13 13:37:39 +0500

    Added noice, nvim-web-devicons, and plenary plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "cpp", "lua", "vim", "vimdoc" },
+            ensure_installed = { "c", "cpp", "bash", "markdown", "markdown_inline", "regex", "lua", "vim", "vimdoc" },
             sync_install = false,
             auto_install = true,
             highlight = {
