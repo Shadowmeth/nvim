fa394d9b46abeb1e5d45517f7d3281962c3f1ce3
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-13 14:05:15 +0500

    Changed yank highlight duration

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -3,6 +3,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight selection on yank",
   group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
   end,
 })
