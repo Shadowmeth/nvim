388d68a94cb3446708fe8dc0edf9cc7555c0c9ee
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-14 07:05:45 +0500

    Cleaned up keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,13 +1,7 @@
 -- Global keybinds (DO NOT SET PLUGIN KEYBINDS HERE!)
 
 -- Save buffer
-vim.keymap.set("n", "<leader>w", function() vim.cmd("w") end, { silent = true, noremap = true})
+vim.keymap.set("n", "<leader>w", function() vim.cmd("w") end, { desc = "Write", silent = true, noremap = true})
 
 -- Quit neovim
-vim.keymap.set("n", "<leader>q", function() vim.cmd("q") end, { silent = true, noremap = true })
-
--- Easier window navigation (like Ctrl-h/j/k/l)
-vim.keymap.set("n", "<C-h>", "<C-w>h", { silent = true, noremap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { silent = true, noremap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { silent = true, noremap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>q", function() vim.cmd("q") end, { desc = "Quit", silent = true, noremap = true })
