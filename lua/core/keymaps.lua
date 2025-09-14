6a680611f94323c0f5e9071ec434564d9d5ba105
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-14 07:16:40 +0500

    Make i go to correct indentation in empty lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,3 +5,9 @@ vim.keymap.set("n", "<leader>w", function() vim.cmd("w") end, { desc = "Write", 
 
 -- Quit neovim
 vim.keymap.set("n", "<leader>q", function() vim.cmd("q") end, { desc = "Quit", silent = true, noremap = true })
+
+-- Auto indent on empty lines
+vim.keymap.set("n", "i",
+    function()
+        return string.match(vim.api.nvim_get_current_line(), '%g') == nil and "\"_cc" or "i"
+    end, { expr = true, silent = true, noremap = true })
