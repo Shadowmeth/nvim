7afc181b9697e602855a8c852a1ad632144b3583
Syed Kumail Hussain Sherazi <47725754+Shadowmeth@users.noreply.github.com>
2025-09-17 15:28:45 +0500

    Sane defaults for diagnostics

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -2,4 +2,7 @@ vim.lsp.enable({ "clangd", "emmylua_ls" })
 
 vim.diagnostic.config({
     virtual_text = { current_line = true },
+    underline = false,
+    update_in_insert = false,
+    signs = true,
 })
